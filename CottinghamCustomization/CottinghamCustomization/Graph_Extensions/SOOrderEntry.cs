d175852f88158cdc40d2d2e15071792db1e9107e
Administrator <Administrator@EC2AMAZ-MOES1C8>
2021-03-19 03:33:49 +0000

    Add cache attached event to override SOOrder salesperson default source.

--- CottinghamCustomization/CottinghamCustomization/Graph_Extensions/SOOrderEntry.cs
+++ CottinghamCustomization/CottinghamCustomization/Graph_Extensions/SOOrderEntry.cs
@@ -1,31 +1,37 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: PX.Objects.SO.SOOrderEntry_Extension
-// Assembly: CottinghamCustomization, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: 1F8F49B4-1D52-44D4-A89E-7C7E4BA7A8DF
-// Assembly location: C:\Program Files\Acumatica ERP\Cottingham\Bin\CottinghamCustomization.dll
-
+﻿using System;
 using PX.Data;
-using System;
+using PX.Objects.EP;
 
 namespace PX.Objects.SO
 {
-  public class SOOrderEntry_Extension : PXGraphExtension<SOOrderEntry>
-  {
-    protected void _(Events.RowPersisting<SOLine> e, PXRowPersisting invokeBaseHandler)
+    public class SOOrderEntry_Extension : PXGraphExtension<SOOrderEntry>
     {
-      if (invokeBaseHandler != null)
-        invokeBaseHandler(e.Cache, e.Args);
-      if (e.Row == null || e.Operation.Command() == PXDBOperation.Delete)
-        return;
-      PXDefaultAttribute.SetPersistingCheck<SOLine.reasonCode>(e.Cache, (object) e.Row, e.Row.IsFree.Value ? PXPersistingCheck.NullOrBlank : PXPersistingCheck.Nothing);
-    }
+        #region Cache Attached
+        [PXMergeAttributes(Method = MergeMethod.Merge)]
+        [PXDefault(typeof(Search<EPEmployee.salesPersonID, Where<EPEmployee.userID, Equal<Current<SOOrder.ownerID>>, 
+                                                                 And<EPEmployee.status, Equal<PX.Objects.AP.Vendor.status.active>>>>), 
+                   PersistingCheck = PXPersistingCheck.Nothing)]
+        protected void _(Events.CacheAttached<SOOrder.salesPersonID> e) { }
+        #endregion
 
-    protected virtual void _(Events.FieldUpdated<SOLine.isFree> e)
-    {
-      SOLine row = e.Row as SOLine;
-      if (!(bool) e.NewValue || !string.IsNullOrEmpty(row.ReasonCode))
-        return;
-      this.Base.Transactions.Cache.RaiseExceptionHandling<SOLine.reasonCode>((object) row, e.NewValue, (Exception) new PXSetPropertyException("This field is mandatory", PXErrorLevel.Warning));
+        #region Event Handlers
+        protected void _(Events.RowPersisting<SOLine> e, PXRowPersisting baseHandler)
+        {
+            baseHandler?.Invoke(e.Cache, e.Args);
+
+            if (e.Row == null || e.Operation.Command() == PXDBOperation.Delete) { return; }
+
+            PXDefaultAttribute.SetPersistingCheck<SOLine.reasonCode>(e.Cache, e.Row, e.Row.IsFree.Value ? PXPersistingCheck.NullOrBlank : PXPersistingCheck.Nothing);
+        }
+
+        protected void _(Events.FieldUpdated<SOLine.isFree> e)
+        {
+            SOLine row = e.Row as SOLine;
+
+            if (!(bool)e.NewValue || !string.IsNullOrEmpty(row.ReasonCode)) { return; }
+
+            this.Base.Transactions.Cache.RaiseExceptionHandling<SOLine.reasonCode>(row, e.NewValue, new PXSetPropertyException(PX.SM.MyMessages.MandatoryField, PXErrorLevel.Warning));
+        }
+        #endregion
     }
-  }
 }
