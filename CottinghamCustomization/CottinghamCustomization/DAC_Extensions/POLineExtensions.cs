6ce917c8f518b19161391b5e6de7eac855eb2ee1
Administrator <Administrator@EC2AMAZ-DVS6HJL>
2021-04-26 03:33:38 +0000

    Adjust layout.

--- CottinghamCustomization/CottinghamCustomization/DAC_Extensions/POLineExtensions.cs
+++ CottinghamCustomization/CottinghamCustomization/DAC_Extensions/POLineExtensions.cs
@@ -1,43 +1,34 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: PX.Objects.PO.POLineExt
-// Assembly: CottinghamCustomization, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: 1F8F49B4-1D52-44D4-A89E-7C7E4BA7A8DF
-// Assembly location: C:\Program Files\Acumatica ERP\Cottingham\Bin\CottinghamCustomization.dll
-
-using PX.Data;
+﻿using PX.Data;
 using PX.Data.BQL;
 using PX.Objects.CS;
 
 namespace PX.Objects.PO
 {
-  public class POLineExt : PXCacheExtension<POLine>
-  {
-    public const string Alcohol = "ALCOHOL";
-
-    [PXString]
-    [PXUIField(DisplayName = "Alc. %", IsReadOnly = true)]
-    [PXUnboundDefault(typeof (Search2<CSAnswers.value, InnerJoinSingleTable<PX.Objects.IN.InventoryItem, On<PX.Objects.IN.InventoryItem.inventoryID, Equal<Current<POLine.inventoryID>>>>, Where<CSAnswers.refNoteID, Equal<PX.Objects.IN.InventoryItem.noteID>, And<CSAnswers.attributeID, Equal<POLineExt.AlcAttr>>>>))]
-    [PXFormula(typeof (Default<POLine.inventoryID>))]
-    public virtual string UsrAlcoholAttr { get; set; }
-
-    [PXString]
-    [PXUIField(DisplayName = "Packaging", IsReadOnly = true)]
-    public virtual string UsrPackaging { get; set; }
-
-    public class AlcAttr : BqlType<IBqlString, string>.Constant<POLineExt.AlcAttr>
+    public class POLineExt : PXCacheExtension<POLine>
     {
-      public AlcAttr()
-        : base("ALCOHOL")
-      {
-      }
-    }
+        public const string Alcohol = "ALCOHOL";
 
-    public abstract class usrAlcoholAttr : BqlType<IBqlString, string>.Field<POLineExt.usrAlcoholAttr>
-    {
-    }
+        #region UsrAlcoholAttr
+        public class AlcAttr : BqlType<IBqlString, string>.Constant<POLineExt.AlcAttr>
+        {
+            public AlcAttr() : base(Alcohol) { }
+        }
 
-    public abstract class usrPackaging : BqlType<IBqlString, string>.Field<POLineExt.usrPackaging>
-    {
+        [PXString]
+        [PXUIField(DisplayName = "Alc. %", IsReadOnly = true)]
+        [PXUnboundDefault(typeof (Search2<CSAnswers.value, InnerJoinSingleTable<PX.Objects.IN.InventoryItem, On<PX.Objects.IN.InventoryItem.inventoryID, Equal<Current<POLine.inventoryID>>>>, 
+                                                           Where<CSAnswers.refNoteID, Equal<PX.Objects.IN.InventoryItem.noteID>, 
+                                                                 And<CSAnswers.attributeID, Equal<POLineExt.AlcAttr>>>>))]
+        [PXFormula(typeof (Default<POLine.inventoryID>))]
+        public virtual string UsrAlcoholAttr { get; set; }
+        public abstract class usrAlcoholAttr : BqlType<IBqlString, string>.Field<POLineExt.usrAlcoholAttr> { }
+        #endregion
+
+        #region UsrPackaging
+        [PXString]
+        [PXUIField(DisplayName = "Packaging", IsReadOnly = true)]
+        public virtual string UsrPackaging { get; set; }
+        public abstract class usrPackaging : BqlType<IBqlString, string>.Field<POLineExt.usrPackaging> { }
+        #endregion
     }
-  }
 }
--- CottinghamCustomization/CottinghamCustomization/DAC_Extensions/SOOrderExtensions.cs
+++ CottinghamCustomization/CottinghamCustomization/DAC_Extensions/SOOrderExtensions.cs
@@ -1,22 +1,22 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: PX.Objects.SO.SO_SOOrder_ExistingColumn
-// Assembly: CottinghamCustomization, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: 1F8F49B4-1D52-44D4-A89E-7C7E4BA7A8DF
-// Assembly location: C:\Program Files\Acumatica ERP\Cottingham\Bin\CottinghamCustomization.dll
-
-using PX.Data;
+﻿using PX.Data;
 using PX.Objects.CT;
 using PX.Objects.PM;
 
 namespace PX.Objects.SO
 {
-  [PXNonInstantiatedExtension]
-  public class SO_SOOrder_ExistingColumn : PXCacheExtension<SOOrder>
-  {
-    [ProjectDefault("SO", typeof (Search<PX.Objects.CR.Location.cDefProjectID, Where<PX.Objects.CR.Location.bAccountID, Equal<Current<SOOrder.customerID>>, And<PX.Objects.CR.Location.locationID, Equal<Current<SOOrder.customerLocationID>>>>>))]
-    [PXRestrictor(typeof (Where<Contract.isCancelled, Equal<False>>), "The {0} project or contract is canceled.", new System.Type[] {typeof (PMProject.contractCD)})]
-    [PXRestrictor(typeof (Where<PMProject.visibleInSO, Equal<True>, Or<PMProject.nonProject, Equal<True>>>), "The '{0}' project is invisible in the module.", new System.Type[] {typeof (PMProject.contractCD)})]
-    [ProjectBase]
-    public int? ProjectID { get; set; }
-  }
+    [PXNonInstantiatedExtension]
+    public class SO_SOOrder_ExistingColumn : PXCacheExtension<SOOrder>
+    {
+        [ProjectDefault(GL.BatchModule.SO, typeof (Search<PX.Objects.CR.Location.cDefProjectID, Where<PX.Objects.CR.Location.bAccountID, Equal<Current<SOOrder.customerID>>, 
+                                                                                                      And<PX.Objects.CR.Location.locationID, Equal<Current<SOOrder.customerLocationID>>>>>))]
+        [PXRestrictor(typeof(Where<PMProject.isCancelled, Equal<False>>), 
+                      PM.Messages.CancelledContract, 
+                      typeof(PMProject.contractCD))]
+        [PXRestrictor(typeof(Where<PMProject.visibleInSO, Equal<True>, Or<PMProject.nonProject, Equal<True>>>), 
+                      PM.Messages.ProjectInvisibleInModule, 
+                      typeof(PMProject.contractCD))]
+        [ProjectBase]
+        //[ProjectBaseAttribute(typeof(SOOrder.customerID))]
+        public int? ProjectID { get; set; }
+    }
 }
