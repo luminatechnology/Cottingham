8a4a27eccf59d562afe42359f2c3ed30bda2645a
Administrator <Administrator@EC2AMAZ-DVS6HJL>
2021-09-26 07:37:42 +0000

    Add new dummy field to do the same logic as PO line.

---
+++ CottinghamCustomization/CottinghamCustomization/DAC_Extensions/RQRequisitionLineExtensions.cs
@@ -0,0 +1,14 @@
+﻿using PX.Data;
+
+namespace PX.Objects.RQ
+{
+    public class RQRequisitionLineExt : PXCacheExtension<PX.Objects.RQ.RQRequisitionLine>
+    {
+        #region UsrPackaging
+        [PXString]
+        [PXUIField(DisplayName = "Packaging", IsReadOnly = true)]
+        public virtual string UsrPackaging { get; set; }
+        public abstract class usrPackaging : PX.Data.BQL.BqlString.Field<usrPackaging> { }
+        #endregion
+    }
+}
---
+++ CottinghamCustomization/CottinghamCustomization/Graph_Extensions/RQRequisitionEntry.cs
@@ -0,0 +1,15 @@
+﻿using PX.Data;
+using PX.Objects.PO;
+
+namespace PX.Objects.RQ
+{
+    public class RQRequisitionEntry_Extension : PXGraphExtension<PX.Objects.RQ.RQRequisitionEntry>
+    {
+        protected void _(Events.FieldSelecting<RQRequisitionLineExt.usrPackaging> e)
+        {
+            if (!(e.Row is RQRequisitionLine row) || !row.InventoryID.HasValue) { return; }
+
+            e.ReturnValue = POOrderEntry_Extension.UOMConversion(Base, row.InventoryID.Value, row.OrderQty.Value, row.UOM);
+        }
+    }
+}
