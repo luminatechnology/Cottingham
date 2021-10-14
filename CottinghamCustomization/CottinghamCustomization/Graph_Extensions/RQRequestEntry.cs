eb41231d47db94f41ed1856a23507bd3f7209b5f
Administrator <Administrator@EC2AMAZ-DVS6HJL>
2021-10-14 13:54:24 +0000

    The vendor price is brought to CuryExtUnitCost field per Adam's request.

--- CottinghamCustomization/CottinghamCustomization/Graph_Extensions/RQRequestEntry.cs
+++ CottinghamCustomization/CottinghamCustomization/Graph_Extensions/RQRequestEntry.cs
@@ -1,7 +1,8 @@
-﻿using System.Linq;
-using PX.Data;
+﻿using PX.Data;
 using PX.Data.BQL;
 using PX.Data.BQL.Fluent;
+using PX.Objects.AP;
+using PX.Objects.CM;
 using PX.Objects.PO;
 
 namespace PX.Objects.RQ
@@ -11,8 +12,6 @@ namespace PX.Objects.RQ
         #region Event Handlers        
         protected void _(Events.FieldUpdated<RQRequestLine.inventoryID> e, PXFieldUpdated baseHandler)
         {
-            baseHandler?.Invoke(e.Cache, e.Args);
-
             var vi = SelectFrom<POVendorInventory>.Where<POVendorInventory.active.IsEqual<True>.And<POVendorInventory.inventoryID.IsEqual<@P.AsInt>>>.View.Select(Base, e.NewValue).TopFirst;
 
             Base.Document.Cache.SetValueExt<RQRequest.vendorID>(Base.Document.Current, vi?.IsDefault == true ? vi?.VendorID : Base.reqclass.Current.GetExtension<RQRequestClassExt>().UsrVendor);
@@ -20,6 +19,32 @@ namespace PX.Objects.RQ
             if (Base.vendor.Current != null)
             {
                 Base.Document.Cache.SetValueExt<RQRequest.curyID>(Base.Document.Current, Base.vendor.Current.CuryID);
+            }
+
+            baseHandler?.Invoke(e.Cache, e.Args);
+        }
+
+        protected void _(Events.FieldDefaulting<RQRequestLine.curyEstUnitCost> e, PXFieldDefaulting baseHandler)
+        {
+            baseHandler?.Invoke(e.Cache, e.Args);
+
+            RQRequestLine row = e.Row as RQRequestLine;
+
+            if (row?.ManualPrice == true && (row?.InventoryID == null || row?.VendorID == null))
+            {
+                e.NewValue = row.CuryEstUnitCost ?? 0m;
+                
+                return;
+            }
+
+            if (row.UOM != null)
+            {
+                CurrencyInfo curyInfo = Base.currencyinfo.Search<CurrencyInfo.curyInfoID>(row.CuryInfoID);
+
+                decimal? vendorUnitCost = APVendorPriceMaint.CalculateUnitCost(e.Cache, row.VendorID, row.VendorLocationID, row.InventoryID, Base.Document.Current.SiteID,
+                                                                               curyInfo, row.UOM, row.OrderQty, Base.Document.Current.OrderDate.Value, row.CuryEstUnitCost);
+
+                e.NewValue = vendorUnitCost;
             }
         }
         #endregion
