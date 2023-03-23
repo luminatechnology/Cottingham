7a2fea4c8dcee62a40db13cf64ca12bc516cfd0a
Administrator <Administrator@EC2AMAZ-DVS6HJL>
2023-03-23 12:23:42 +0800

    Add the existing field to the grid and modify the PXSelectorAttribute to make the navigation work properly.

---
+++ CottinghamCustomization/Graph_Extensions/InventoryTranHistEnq.cs
@@ -0,0 +1,14 @@
+﻿using PX.Data;
+
+namespace PX.Objects.IN
+{
+    public class InventoryTranHistEnq_Extension : PXGraphExtension<InventoryTranHistEnq>
+    {
+        #region Cacche Attached
+        [PXDBString(15, IsUnicode = true)]
+        [PXUIField(DisplayName = "SO Shipment Nbr.", Visible = false, Visibility = PXUIVisibility.Visible)]
+        [PXSelector(typeof(Search<SO.SOShipment.shipmentNbr>))]
+        protected virtual void _(Events.CacheAttached<INTran.sOShipmentNbr> e) { }
+        #endregion
+    }
+}
